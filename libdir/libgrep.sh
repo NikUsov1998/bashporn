2d53c3be31876dbe0a665ea07c6638449fcd4183
Nick <nik_usov.1998@mail.ru>
2026-03-31 21:55:13 +0500

    add ignore case function in libgrep

--- libdir/libgrep.sh
+++ libdir/libgrep.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 echo libgrep
+
+grep_ignore_case()
+{
+	local pattern="$1"
+	local file="$2"
+
+	if [[ -z "$pattern" ]]; then
+		echo "pattern is required!"
+		return 1
+	fi
+
+	if [[ -n "$file" ]] && [[ ! -f "$file" ]]; then
+		echo "file not found!"
+		return 1
+	fi
+
+	grep -i "$pattern" "$file"
+
+}
+
+
